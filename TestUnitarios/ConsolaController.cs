91ca4b2b32d78f29fe44ffe98c70f23b546698ef
minnahei <minna.s.heinonen@gmail.com>
2019-07-27 13:22:22 +0200

    July 27th

---
+++ TestUnitarios/ConsolaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace TestUnitarios
+{
+    public class ConsolaController
+    {
+        public string Salute()
+        {
+            return "Hola mundo";
+        }
+
+        public string SaluteTo(string name)
+        {
+            if(name is null)
+            {
+                throw new NullReferenceException("Nombre NULO!!!");
+            }
+            if(name == "")
+            {
+                return "Dime quien eres";
+            }
+            return $"Hola {name}"; //"Hola "+name
+        }
+    }
+}
---
+++ TestUnitarios/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestUnitarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsolaController mgr = new ConsolaController();
+            Console.WriteLine(mgr.Salute());
+        }
+    }
+}
